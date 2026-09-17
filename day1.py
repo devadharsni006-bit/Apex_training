c77395613d38c1544f962cc4abb6668f6e757e9b
devadharsni006-bit <devadharsni006@gmail.com>
2026-09-17 14:56:09 +0530

    ARITHMETIC OPERATOR
    ARITHMETIC OPERATOR

---
+++ day1.py
@@ -0,0 +1,15 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("\nArithmetic Operations")
+print("Addition       :", a + b)
+print("Subtraction    :", a - b)
+print("Multiplication :", a * b)
+print("Division       :", a / b)
+print("Modulus        :", a % b)
+print("Floor Division :", a // b)
+print("Exponentiation :", a ** b)
+
+
+
+
